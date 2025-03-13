3a50c83ba4086f4e1c377fc92a47af84d2470665
Chiara Rinzivillo <chiararinzi@gmail.com>
2025-03-13 13:25:28 +0000

    State added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from "react";
+//import React from "react";
 //import ReactDOM from "react-dom/client";
 import "./index.css";
-import Card from "./Card";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import { robots } from "./robots";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
@@ -23,11 +21,7 @@ root.render(
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    </div>
+    <App />
   </StrictMode>
 );
 
